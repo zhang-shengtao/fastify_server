31e94cd6618e2387df6e3b9cd283708d2329bac2
tao.sheng <tao.sheng@2077.ai>
2026-06-11 14:42:20 +0800

    去掉打包后的map文件

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -10,7 +10,7 @@ export default class AuthService {
     const users = await this.fastify.db.select().from(admin_user).where(eq(admin_user.account, username));
     if (users.length === 0) throw new AppError(BusinessCode.PARAM_ERROR, "账号或密码错误");
     const item = users.find((item) => item.password === password);
-    if (!item) throw new AppError(BusinessCode.PARAM_ERROR, "账号或密码错误");
+    if (!item) throw new AppError(BusinessCode.PARAM_ERROR, "账号或密码错误.");
     const uid = randomUUID();
     const token = await this.fastify.jwt.admin.sign({ uid });
     const decode = this.fastify.jwt.admin.decode<DecodedJwtPayload>(token);
